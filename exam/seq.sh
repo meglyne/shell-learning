31b5a17bfcd0811111103c4e52e769de14c4dbdb
Margot Marc <margot.marc@outlook.fr>
2020-04-06 19:24:36 +0200

    added most of the code :|

--- exam/seq.sh
+++ exam/seq.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-if [ $# -eq 3  ]; then
-	echo "3 arguments" 
+message="Usage:$0 FIRST INCREMENT LAST"
+
+if [ "$#" -eq 3  ]; then
+	if [ "$3" -gt "$1" -a "$2" -gt 0 -o "$3" -lt "$1" -a "$2" -lt 0 ]; then
+		i="$1"
+		while [ "$i" -ge "$3" -a "$i" -le "$3" ]; do
+			echo "$i"	
+			i=$(("$i"+"$2"))
+		done
+	elif [ "$3" -eq "$1" ]; then
+		echo "$1"
+		exit 0
+	else
+		echo "$message"
+		exit 1
+	fi	
 else
-	echo "Usage:$0 FIRST INCREMENT LAST"
+	echo "$message" 
 	exit 1
 fi
