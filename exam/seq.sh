a9875115341bd0d9bc50f20891232d9454113627
Margot Marc <margot.marc@outlook.fr>
2020-04-12 03:02:56 +0200

    Added comments

--- exam/seq.sh
+++ exam/seq.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
 message="Usage:$0 FIRST INCREMENT LAST"
-
+# if there are 3 parameters
 if [ "$#" -eq 3 ]; then
+	# if FIRST is less than LAST
 	if [ $3 -gt $1 ]; then
+		# if INCREMENT is positive
 		if [ $2 -gt 0 ]; then
 			i=$1
 			while [ $i -le $3 ]; do
 				echo $i
 				i=$(($i + $2))
 			done
+			#exit with code 0
 			exit 0
+		# if INCREMENT is negative
 		else
+			#exit with an error
 			exit 1
 		fi
-
+	# if FIRST is greater than LAST
 	elif [ $3 -lt $1 ]; then
+		# if INCREMENT is negative
 		if [ $2 -lt 0 ]; then
 			i=$1
 			while [ $i -ge $3 ]; do
@@ -23,6 +29,7 @@ if [ "$#" -eq 3 ]; then
 				i=$(($i + $2))
 			done
 			exit 0
+		#if INCREMENT is positive
 		else
 			exit 1
 		fi
@@ -31,6 +38,7 @@ if [ "$#" -eq 3 ]; then
 		echo $1
 		exit 0
 	fi
+# if the number of arguments is incorrect
 else
 	echo "$message"
 	exit 1
