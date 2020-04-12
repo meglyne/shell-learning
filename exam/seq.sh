e93ad21a46eec4f5300119812574dad7984650c4
Margot Marc <margot.marc@outlook.fr>
2020-04-12 02:58:35 +0200

    Finished first version of script

--- exam/seq.sh
+++ exam/seq.sh
@@ -2,21 +2,36 @@
 
 message="Usage:$0 FIRST INCREMENT LAST"
 
-if [ "$#" -eq 3  ]; then
-	if [ "$3" -gt "$1" -a "$2" -gt 0 -o "$3" -lt "$1" -a "$2" -lt 0 ]; then
-		i="$1"
-		while [ "$i" -ge "$3" -a "$i" -le "$3" ]; do
-			echo "$i"	
-			i=$(("$i"+"$2"))
-		done
-	elif [ "$3" -eq "$1" ]; then
-		echo "$1"
-		exit 0
+if [ "$#" -eq 3 ]; then
+	if [ $3 -gt $1 ]; then
+		if [ $2 -gt 0 ]; then
+			i=$1
+			while [ $i -le $3 ]; do
+				echo $i
+				i=$(($i + $2))
+			done
+			exit 0
+		else
+			exit 1
+		fi
+
+	elif [ $3 -lt $1 ]; then
+		if [ $2 -lt 0 ]; then
+			i=$1
+			while [ $i -ge $3 ]; do
+				echo $i
+				i=$(($i + $2))
+			done
+			exit 0
+		else
+			exit 1
+		fi
+	#if FIRST and LAST are equal
 	else
-		echo "$message"
-		exit 1
-	fi	
+		echo $1
+		exit 0
+	fi
 else
-	echo "$message" 
+	echo "$message"
 	exit 1
 fi
